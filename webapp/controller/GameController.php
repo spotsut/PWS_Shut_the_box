670bbecf6293cf34681b5b497e1a82e901e3a867
Alexander Scorpion <36804913+AlexScorpion100@users.noreply.github.com>
2020-05-12 09:29:44 +0100

    User

---
+++ webapp/controller/GameController.php
@@ -0,0 +1,10 @@
+<?php
+use ArmoredCore\Controllers\BaseController;
+use ArmoredCore\WebObjects\Redirect;
+use ArmoredCore\WebObjects\Session;
+use ArmoredCore\WebObjects\View;
+
+class GameController
+{
+
+}
