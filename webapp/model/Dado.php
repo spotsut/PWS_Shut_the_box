94e0a2f5e87c3eab1938f415f316d458d47a9929
Alexander Scorpion <36804913+AlexScorpion100@users.noreply.github.com>
2020-05-12 11:06:53 +0100

    Update Dado.php

--- webapp/model/Dado.php
+++ webapp/model/Dado.php
@@ -3,13 +3,9 @@
 
 class Dado
 {
-<<<<<<< Updated upstream
     public function rolarDado(){
         $dado = 0;
-=======
-    public function dado(){
         $dado = rand (1,6) ;
->>>>>>> Stashed changes
         return $dado;
     }
 }
