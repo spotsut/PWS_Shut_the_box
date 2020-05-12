610c7b029038256fe617af848f2c4903462baa22
Alexander Scorpion <36804913+AlexScorpion100@users.noreply.github.com>
2020-05-12 11:01:01 +0100

    register

--- webapp/controller/UserController.php
+++ webapp/controller/UserController.php
@@ -3,12 +3,17 @@ use ArmoredCore\Controllers\BaseController;
 use ArmoredCore\WebObjects\Redirect;
 use ArmoredCore\WebObjects\Session;
 use ArmoredCore\WebObjects\View;
+use ArmoredCore\WebObjects\Post;
+
 
 class UserController
 {
     public function register (){
         $msg = new Register();
-        $msg->register();
+        $nick = Post::get('nick');
+        $email = Post::get('email');
+        $pass = Post::get('password');
+        $msg = $msg->register_user($nick, $email, $pass);
 
         return View::make('user.register', ['msg' => $msg]);
     }
--- webapp/model/Register.php
+++ webapp/model/Register.php
@@ -2,8 +2,31 @@
 
 class Register
 {
-    function register($nick, $email, $password)
+    public function register_user($nick, $email, $pass)
     {
-        return "pila";
+
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "mydb";
+
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        $sql = "INSERT INTO users (nickname, email, password) VALUES ('$nick', '$email', '$pass')";
+
+        if ($conn->query($sql) === TRUE) {
+            $msg =  "New record created successfully";
+        } else {
+            $msg = "Error: " . $sql . "<br>" . $conn->error;
+        }
+
+        $conn->close();
+
+        return $msg;
     }
 }
