ad141c891bdda72976d7f33dda60a9b7535ac9f7
Alexander Scorpion <36804913+AlexScorpion100@users.noreply.github.com>
2020-05-14 13:00:07 +0100

    game engine

--- webapp/model/NumerosBloqueio.php
+++ webapp/model/NumerosBloqueio.php
@@ -5,12 +5,19 @@ class NumerosBloqueio
 {
     private $numeros = [];
 
-    public function iniciars(){
-
+    public function iniciar(){
+        $this->numeros = [];
     }
 
     public function bloquearNumeros($numeros , $somaDados){
+        for ($i = 0; $i> sizeof($this->numero); $i++){
+            if($this->numeros[$i] == $numeros[0]){
 
+            }elseif($this->numeros[$i] == $numeros[1]){
+
+            }
+        }
+        
         return true;
     }
 
