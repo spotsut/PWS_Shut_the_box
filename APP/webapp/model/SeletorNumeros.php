e2c7b59c2dc9a3aee771ec538dda790bd0a7aa65
Tomé <tome.nunes80@gmail.com>
2020-05-28 11:38:40 +0100

    seletor

--- APP/webapp/model/SeletorNumeros.php
+++ APP/webapp/model/SeletorNumeros.php
@@ -7,15 +7,34 @@ class SeletorNumeros
 
     public function  validadeNumber($userNumber, $numerosBloqueio)
     {
+        if ($numerosBloqueio[$userNumber] == true){
+            return false;
 
+        }else
+        {
+            return true;
+        }
     }
 
     public function updateSelection($userNumber){
+        if(in_array($userNumber,$this->numerosSelecionados)==true){
 
+        }else
+        {
+            array_push($this->numerosSelecionados,$userNumber);
+        }
     }
 
     public function checkSeletionTotal($totalDados){
+    $soma = 0;
+    foreach ($this->numerosSelecionados as $numero){
+        $soma+=$numero;
 
+    }
+    if ($soma == $totalDados){
+        return true;
+
+    }
     }
 
     public function clearSelection(){
@@ -23,6 +42,6 @@ class SeletorNumeros
     }
 
     public function selectionHasNumber($number) {
-
+        return in_array($number,$this->numerosSelecionados);
     }
 }
